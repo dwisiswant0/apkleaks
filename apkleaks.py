aa1fcbf279e94df18ce1022677a4f947d307a00b
dw1 <dwi.siswanto98@gmail.com>
2020-05-29 16:31:32 +0700

    First release candidate

---
+++ apkleaks.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from apkleaks.apkleaks import apkleaks
+from apkleaks.colors import colors
+import argparse
+import shutil
+
+def header():
+	VERSION = open("VERSION", "r").read()
+	return ("     _    ____  _  ___               _        \n    / \\  |  _ \\| |/ / |    ___  __ _| | _____ \n   / _ \\ | |_) | ' /| |   / _ \\/ _` | |/ / __|\n  / ___ \\|  __/| . \\| |__|  __/ (_| |   <\\__ \\\n /_/   \\_\\_|   |_|\\_\\_____\\___|\\__,_|_|\\_\\___/\n # {}\n --\n Scanning APK file for secrets\n (c) 2020, dwisiswant0\n".format(VERSION))
+
+def argument():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-f", "--file", help="APK file to scanning", type=str, required=True)
+	arg = parser.parse_args()
+	return arg
+
+if __name__ == "__main__":
+	print(colors.HEADER)
+	print(colors.HEADER + header() + colors.ENDC)
+	arg = argument()
+	init = apkleaks(arg.file)
+	apk = init.integrity()
+	out = init.decompile()
+	init.scanning(out)
+	shutil.rmtree(out)
---
+++ apkleaks/apkleaks.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+from apk_parse.apk import APK
+from colors import colors
+from distutils.spawn import find_executable
+import json
+import numpy
+import os
+import re
+import tempfile
+
+class apkleaks:
+	def __init__(self, file):
+		self.file = file
+
+	def apk_info(self):
+		return APK(self.file)
+
+	def integrity(self):
+		if find_executable("apktool") is None:
+			exit(colors.WARNING + "Can't find 'apktool'. Please see https://ibotpeaches.github.io/Apktool/install/" + colors.ENDC)
+
+		if os.path.isfile(self.file) is True:
+			try:
+				global apk
+				apk = self.apk_info()
+			except Exception as e:
+				exit(colors.WARNING + str(e) + colors.ENDC)
+			else:
+				return apk
+		else:
+			exit(colors.WARNING + "It's not a file!" + colors.ENDC)
+
+	def decompile(self):
+		out = tempfile.mkdtemp(prefix="apkleaks-")
+		print("%s** Decompiling APK...%s" % (colors.OKBLUE, colors.ENDC))
+		os.system("apktool d %s  -fro %s" % (self.file, out))
+		return out
+
+	def unique(self, list): 
+	    x = numpy.array(list) 
+	    return (numpy.unique(x))
+
+	def finder(self, pattern, path):
+		TEXTCHARS = ''.join(map(chr, [7,8,9,10,12,13,27] + range(0x20, 0x100)))
+		is_binary_string = lambda bytes: bool(bytes.translate(None, TEXTCHARS))
+		matcher = re.compile(pattern)
+		found = []
+		for path, _, files in os.walk(path):
+			for fn in files:
+				filepath = os.path.join(path, fn)
+				if is_binary_string(open(filepath).read(1024)):
+					continue
+				with open(filepath) as handle:
+					for lineno, line in enumerate(handle):
+						mo = matcher.search(line)
+						if mo:
+							found.append(mo.group())
+		return self.unique(found)
+
+	def scanning(self, path):
+		print("%s\n** Scanning against '%s' (v%s)%s" % (colors.OKBLUE, apk.get_package(), apk.get_androidversion_name(), colors.ENDC))
+		with open("config/regexes.json") as regexes:
+			regex = json.load(regexes)
+			for name, pattern in regex.items():
+				found = self.finder(pattern, path)
+				if len(found):
+					print("%s\n[%s]%s" % (colors.OKGREEN, name, colors.ENDC))
+					for secret in found:
+						if name == "LinkFinder" and re.match(r"^.L[a-z].+\/.+", secret) is not None:
+							continue
+						print("- %s" % (secret))
