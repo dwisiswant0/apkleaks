36c214dd5ffaa1d955326deb9e34f5dce0e2eec1
dw1 <dwi.siswanto98@gmail.com>
2020-05-30 12:02:10 +0700

    :page_facing_up: Changing header descriptions

--- apkleaks.py
+++ apkleaks.py
@@ -6,7 +6,7 @@ import os
 
 def header():
 	VERSION = open(os.path.dirname(os.path.realpath(__file__)) + "/VERSION", "r").read()
-	return ("     _    ____  _  ___               _        \n    / \\  |  _ \\| |/ / |    ___  __ _| | _____ \n   / _ \\ | |_) | ' /| |   / _ \\/ _` | |/ / __|\n  / ___ \\|  __/| . \\| |__|  __/ (_| |   <\\__ \\\n /_/   \\_\\_|   |_|\\_\\_____\\___|\\__,_|_|\\_\\___/\n # {}\n --\n Scanning APK file for secrets\n (c) 2020, dwisiswant0\n".format(VERSION))
+	return ("     _    ____  _  ___               _        \n    / \\  |  _ \\| |/ / |    ___  __ _| | _____ \n   / _ \\ | |_) | ' /| |   / _ \\/ _` | |/ / __|\n  / ___ \\|  __/| . \\| |__|  __/ (_| |   <\\__ \\\n /_/   \\_\\_|   |_|\\_\\_____\\___|\\__,_|_|\\_\\___/\n # {}\n --\n Scanning APK file for URIs, endpoints & secrets\n (c) 2020, dwisiswant0\n".format(VERSION))
 
 def argument():
 	parser = argparse.ArgumentParser()
