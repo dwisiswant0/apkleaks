7b9c0dcf7c38921cefb2cebe8537d86b370477da
Dwi Siswanto <dwi.siswanto98@gmail.com>
2021-03-21 06:51:13 +0700

    Add author_email

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from setuptools import setup
 
 install_requires = open('requirements.txt', 'r').readlines()
@@ -10,7 +11,7 @@ setup(
     url='https://github.com/dwisiswant0/apkleaks/',
     license='Apache License 2.0',
     author='dwisiswant0',
-    author_email='',
+    author_email='me@dw1.io',
     description='Scanning APK file for URIs, endpoints & secrets.',
     install_requires=install_requires,
     scripts=['apkleaks.py'],
