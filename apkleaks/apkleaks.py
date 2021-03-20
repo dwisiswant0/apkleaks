3745501638ab3cd077f5586029f87679bbc73451
Dwi Siswanto <dwi.siswanto98@gmail.com>
2021-03-21 05:33:44 +0700

    Using join path & pathlib

--- apkleaks/apkleaks.py
+++ apkleaks/apkleaks.py
@@ -11,6 +11,7 @@ import threading
 
 from contextlib import closing
 from distutils.spawn import find_executable
+from pathlib import Path
 from pipes import quote
 from urllib.request import urlopen
 from zipfile import ZipFile
@@ -30,8 +31,8 @@ class APKLeaks:
 		self.main_dir = os.path.dirname(os.path.realpath(__file__))
 		self.output = tempfile.mkstemp(suffix=".%s" % ("json" if self.json is True else "txt"), prefix=self.prefix)[1] if args.output is None else args.output
 		self.fileout = open(self.output, "%s" % ("w" if self.json is True else "a"))
-		self.pattern = self.main_dir + "/../config/regexes.json" if args.pattern is None else args.pattern
-		self.jadx = find_executable("jadx") if find_executable("jadx") is not None else self.main_dir + "/../jadx/bin/jadx%s" % (".bat" if os.name == "nt" else "")
+		self.pattern = os.path.join(str(Path(self.main_dir).parent), "config", "regexes.json") if args.pattern is None else args.pattern
+		self.jadx = find_executable("jadx") if find_executable("jadx") is not None else os.path.join(str(Path(self.main_dir).parent), "jadx", "bin", "jadx%s" % (".bat" if os.name == "nt" else ""))
 		self.out_json = {}
 		self.scanned = False
 		logging.config.dictConfig({"version": 1, "disable_existing_loggers": True})
