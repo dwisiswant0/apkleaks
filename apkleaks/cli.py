92d0bf6446ba1755554b80d901e6533cdb215f69
Dwi Siswanto <dwi.siswanto98@gmail.com>
2021-03-23 00:31:35 +0700

    Add leading 'v' for version

--- apkleaks/cli.py
+++ apkleaks/cli.py
@@ -11,7 +11,7 @@ from apkleaks.colors import color as col
 
 def header():
 	try:
-		VERSION = pkg_resources.require("apkleaks")[0].version
+		VERSION = "v" + pkg_resources.require("apkleaks")[0].version
 	except Exception:
 		VERSION = open(os.path.join(str(Path(__file__).parent.parent), "VERSION"), "r").read().strip()
 	print(col.HEADER + "     _    ____  _  ___               _        \n    / \\  |  _ \\| |/ / |    ___  __ _| | _____ \n   / _ \\ | |_) | ' /| |   / _ \\/ _` | |/ / __|\n  / ___ \\|  __/| . \\| |__|  __/ (_| |   <\\__ \\\n /_/   \\_\\_|   |_|\\_\\_____\\___|\\__,_|_|\\_\\___/\n {}\n --\n Scanning APK file for URIs, endpoints & secrets\n (c) 2020-2021, dwisiswant0\n".format(VERSION) + col.ENDC, file=sys.stderr)
