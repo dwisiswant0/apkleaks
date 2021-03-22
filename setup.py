2edd30a2f31c7d969f9f42fb1c4da7b5ad50380c
Dwi Siswanto <dwi.siswanto98@gmail.com>
2021-03-23 00:37:36 +0700

    Using double-quote instead

--- setup.py
+++ setup.py
@@ -1,33 +1,33 @@
 #!/usr/bin/env python3
 from setuptools import setup, find_packages
 
-install_requires = open('./requirements.txt', 'r').readlines()
-version = open('./VERSION', 'r').read().strip()
-long_description = open('./README.md', 'r', encoding='utf-8').read()
-packages = find_packages(exclude=['*.tests', '*.tests.*', 'test*', 'tests'])
-packages.append('config')
+install_requires = open("./requirements.txt", "r").readlines()
+version = open("./VERSION", "r").read().strip()
+long_description = open("./README.md", "r", encoding="utf-8").read()
+packages = find_packages(exclude=["*.tests", "*.tests.*", "test*", "tests"])
+packages.append("config")
 
 setup(
-    name='apkleaks',
+    name="apkleaks",
     version=version,
     packages=packages,
-    url='https://github.com/dwisiswant0/apkleaks/',
-    license='Apache License 2.0',
-    author='dwisiswant0',
-    author_email='me@dw1.io',
-    description='Scanning APK file for URIs, endpoints & secrets.',
+    url="https://github.com/dwisiswant0/apkleaks/",
+    license="Apache License 2.0",
+    author="dwisiswant0",
+    author_email="me@dw1.io",
+    description="Scanning APK file for URIs, endpoints & secrets.",
     long_description=long_description,
     install_requires=install_requires,
     include_package_data=True,
     entry_points={
-        'console_scripts': [
+        "console_scripts": [
             "apkleaks=apkleaks.cli:main",
         ]
     },
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Natural Language :: English',
-        'Topic :: Security',
-        'Topic :: Utilities',
+        "Development Status :: 3 - Alpha",
+        "Natural Language :: English",
+        "Topic :: Security",
+        "Topic :: Utilities",
     ]
 )
