dce9c92433035a371731226ea2b93255c2b52f24
Shivasurya <s5sankarapandian@uwaterloo.ca>
2023-04-05 00:08:06 -0400

    improved scan speed by caching files computing check

--- apkleaks/utils.py
+++ apkleaks/utils.py
@@ -14,18 +14,18 @@ class util:
 		util.write(message + "\n", color)
 
 	@staticmethod
-	def finder(pattern, path):
+	def finder(pattern, files_to_scan):
 		matcher = re.compile(pattern)
 		found = []
-		for fp, _, files in os.walk(path):
-			for fn in files:
-				filepath = os.path.join(fp, fn)
-				with open(filepath) as handle:
-					try:
-						for line in handle.readlines():
-							mo = matcher.search(line)
-							if mo:
-								found.append(mo.group())
-					except Exception:
-						pass
+		
+		for file in files_to_scan:
+			with open(file) as handle:
+				try:
+					for line in handle.readlines():
+						mo = matcher.search(line)
+						if mo:
+							found.append(mo.group())
+				except Exception:
+					pass
+
 		return sorted(list(set(found)))
