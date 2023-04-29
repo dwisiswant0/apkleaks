77d7c946bb894948cb56cd086922eac5907c2f0f
rakshith111 <rakshithbabu111@gmail.com>
2023-04-29 13:43:03 +0530

    Add Auto Update to latest Update Console write statements

--- apkleaks/apkleaks.py
+++ apkleaks/apkleaks.py
@@ -54,7 +54,8 @@ class APKLeaks:
 		response = requests.get(url)
 
 		if response.status_code != 200:
-			util.writeln(f"Error fetching latest release info: {response.status_code}", col.FAIL)
+			util.writeln(
+				f"[-] Error fetching latest release info: {response.status_code}", col.FAIL)
 			return
 
 		release_info = response.json()
@@ -82,7 +83,7 @@ class APKLeaks:
 					break
 
 			if not download_url:
-				util.writeln(" Error: Download URL not found.", col.FAIL)
+				util.writeln("[-] Error: Download URL not found.", col.FAIL)
 				return
 
 			response = requests.get(download_url, stream=True)
@@ -110,19 +111,33 @@ class APKLeaks:
 
 			util.writeln(f"[+] jadx-{tag_name} successfully downloaded and extracted.", col.OKGREEN)
 		else:
-			util.writeln(f"[+] jadx-{tag_name} is already up to date.", col.OKGREEN)
+			util.writeln(f"[*] jadx-{tag_name} is already up to date.", col.OKGREEN)
 
 	def integrity(self):
 		if os.path.exists(self.jadx) is False:
-			util.writeln("Can't find jadx binary.", col.WARNING)
-			util.writeln("\n** Updating to latest jadx...\n", col.OKBLUE)
+			util.writeln("[-] Can't find jadx binary.", col.WARNING)
+			util.writeln("\n[+] Updating to latest jadx...\n", col.OKBLUE)
 			self.download_latest_jadx(self.jadx)
 		else:
 			version_file = os.path.join("jadx", "latest_version.txt")
 			if os.path.exists(version_file):
 				with open(version_file, "r") as f:
 					current_version = f.read().strip()
-			util.writeln("Jadx version: %s" % (current_version), col.OKBLUE)
+			util.writeln("[*] Jadx version: %s" % (current_version), col.OKBLUE)
+			url = "https://api.github.com/repos/skylot/jadx/releases/latest"
+			response = requests.get(url)
+
+			if response.status_code != 200:
+				util.writeln(f"[-] Error fetching latest release info: {response.status_code}", col.FAIL)
+				util.writeln("\n[+] Continuing with existing version\n", col.OKBLUE)
+				return
+
+			release_info = response.json()
+			tag_name = release_info["tag_name"]
+			if tag_name != current_version:
+				util.writeln("\n[+] New update found\n[+]Updating to latest jadx...\n", col.OKBLUE)
+				self.download_latest_jadx(self.jadx)
+			
 
 
 		if os.path.isfile(self.file):
@@ -134,10 +149,10 @@ class APKLeaks:
 			else:
 				return self.apk
 		else:
-			sys.exit(util.writeln("It's not a valid file!", col.WARNING))
+			sys.exit(util.writeln("[-] It's not a valid file!", col.WARNING))
 
 	def decompile(self):
-		util.writeln("** Decompiling APK...", col.OKBLUE)
+		util.writeln("[+] Decompiling APK...", col.OKBLUE)
 		args = [self.jadx, self.file, "-d", self.tempdir]
 		try:
 			args.extend(re.split(r"\s|=", self.disarg))
@@ -166,8 +181,8 @@ class APKLeaks:
 
 	def scanning(self):
 		if self.apk is None:
-			sys.exit(util.writeln("** Undefined package. Exit!", col.FAIL))
-		util.writeln("\n** Scanning against '%s'" % (self.apk.package), col.OKBLUE)
+			sys.exit(util.writeln("[+] Undefined package. Exit!", col.FAIL))
+		util.writeln("\n[+] Scanning against '%s'" % (self.apk.package), col.OKBLUE)
 		self.out_json["package"] = self.apk.package
 		self.out_json["results"] = []
 		with open(self.pattern) as regexes:
@@ -179,21 +194,21 @@ class APKLeaks:
 							thread = threading.Thread(target = self.extract, args = (name, util.finder(p, self.tempdir)))
 							thread.start()
 						except KeyboardInterrupt:
-							sys.exit(util.writeln("\n** Interrupted. Aborting...", col.FAIL))
+							sys.exit(util.writeln("\n[+] Interrupted. Aborting...", col.FAIL))
 				else:
 					try:
 						thread = threading.Thread(target = self.extract, args = (name, util.finder(pattern, self.tempdir)))
 						thread.start()
 					except KeyboardInterrupt:
-						sys.exit(util.writeln("\n** Interrupted. Aborting...", col.FAIL))
+						sys.exit(util.writeln("\n[+] Interrupted. Aborting...", col.FAIL))
 
 	def cleanup(self):
 		shutil.rmtree(self.tempdir)
 		if self.scanned:
 			self.fileout.write("%s" % (json.dumps(self.out_json, indent=4) if self.json else ""))
 			self.fileout.close()
-			print("%s\n** Results saved into '%s%s%s%s'%s." % (col.HEADER, col.ENDC, col.OKGREEN, self.output, col.HEADER, col.ENDC))
+			print("%s\n[+] Results saved into '%s%s%s%s'%s." % (col.HEADER, col.ENDC, col.OKGREEN, self.output, col.HEADER, col.ENDC))
 		else:
 			self.fileout.close()
 			os.remove(self.output)
-			util.writeln("\n** Done with nothing. ¯\\_(ツ)_/¯", col.WARNING)
+			util.writeln("\n[+] Done with nothing. ¯\\_(ツ)_/¯", col.WARNING)
